933c3f46f3170de173e97e40e68a582bfdb0db5f
Sho Sakai <show75@ezweb.ne.jp>
2021-01-11 21:11:28 +0900

    typeORM

---
+++ day9-typeorm/src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { User } from './users/users.entity';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'lastrust',
+      password: 'postgres',
+      database: 'postgres',
+      entities: [User],
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
