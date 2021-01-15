8cdbbc7bc2a760ac70843f3127207416579b7b73
Sho Sakai <show75@ezweb.ne.jp>
2021-01-15 20:46:59 +0900

    feat: login function which generates and returns JWT

--- rest-authentication/src/auth/auth.controller.ts
+++ rest-authentication/src/auth/auth.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Request, Post, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './local-auth.gurds';
 
 @Controller()
 export class AuthController {
+  constructor(private authService: AuthService) {}
+
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
-    return req.user;
+    return this.authService.login(req.user);
   }
 }
